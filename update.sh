912e080a0b6d7801c3a063382db64b3cba5ff573
hotnuma <hotnuma@gmail.com>
2025-04-16 11:09:08 +0200

    fix missing translations

--- update.sh
+++ update.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-domain="imgview"
-potfile="po/$domain.pot"
+domain_opt="imgview"
+potfile_opt="po/$domain_opt.pot"
+
+error_exit()
+{
+    msg="$1"
+    test "$msg" != "" || msg="an error occurred"
+    printf "*** $msg\nabort...\n"
+    exit 1
+}
 
 echo "*** creating pot file"
-rm -f $potfile
+rm -f $potfile_opt
 xgettext \
 	--copyright-holder="2025 hotnuma" \
 	--package-name="Hello" \
 	--package-version="1.0" \
 	--msgid-bugs-address="hotnuma@gmail.com" \
 	--no-wrap \
-	--keyword=_ \
 	--from-code=UTF-8 \
-	-s -d $domain -f "po/POTFILES.in" -o "$potfile"
+	-k_ -kN_ \
+	-s -d $domain_opt -f "po/POTFILES.in" -o "$potfile_opt"
+
+test $? -eq 0 || error_exit "xgettext failed"
 
 # create languages
 linguas="po/LINGUAS"
@@ -23,15 +33,15 @@ while read -r line; do
     if [ ! -e "$pofile" ]; then
         echo "*** creating $pofile"
         msginit --no-translator --no-wrap --locale="$lang.UTF-8" \
-        --input="$potfile" --output-file="$pofile"
+        --input="$potfile_opt" --output-file="$pofile"
     fi
 done < "$linguas"
 
 # update languages
 for file in po/*.po
 do
-    echo "*** updating $file from $potfile"
-	msgmerge --sort-output --no-wrap --update --backup=off $file $potfile
+    echo "*** updating $file from $potfile_opt"
+	msgmerge --sort-output --no-wrap --update --backup=off $file $potfile_opt
 done
 
 
