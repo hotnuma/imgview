ecc420e8bcf7d9e0d68f9ab94d1b9644fcadf7c8
hotnuma <hotnuma@gmail.com>
2025-04-18 14:25:45 +0200

    sepia filter

--- window.c
+++ window.c
@@ -130,9 +130,11 @@ static void _window_hide_cursor(VnrWindow *window);
 static void _window_show_cursor(VnrWindow *window);
 static void _window_action_properties(VnrWindow *window, GtkWidget *widget);
 static void _window_action_preferences(VnrWindow *window, GtkWidget *widget);
+
 static void _window_action_help(VnrWindow *window, GtkWidget *widget);
 static void _window_action_test(VnrWindow *window, GtkWidget *widget);
 static void _window_filter_grayscale(VnrWindow *window);
+static void _window_filter_sepia(VnrWindow *window);
 static GdkPixbuf* _window_pixbuf_new(VnrWindow *window);
 
 // private Actions ------------------------------------------------------------
@@ -374,7 +376,7 @@ static void _window_action_help(VnrWindow *window, GtkWidget *widget)
 {
     g_return_if_fail(window != NULL);
 
-    _window_filter_grayscale(window);
+    _window_filter_sepia(window);
 
     return;
 
@@ -420,6 +422,62 @@ static void _window_filter_grayscale(VnrWindow *window)
             dest_p[0] = (int) level;
             dest_p[1] = (int) level;
             dest_p[2] = (int) level;
+
+            if (has_alpha)
+                dest_p[3] = src_p[3];
+        }
+    }
+
+    _window_view_set_static(window, dest_pixbuf);
+}
+
+static void _window_filter_sepia(VnrWindow *window)
+{
+    if (!window->can_edit)
+        return;
+
+    GdkPixbuf *src_pixbuf = uni_image_view_get_pixbuf(
+                            UNI_IMAGE_VIEW(window->view));
+
+    GdkPixbuf *dest_pixbuf = _window_pixbuf_new(window);
+
+    int width = gdk_pixbuf_get_width(dest_pixbuf);
+    int height = gdk_pixbuf_get_height(dest_pixbuf);
+    int stride = gdk_pixbuf_get_rowstride(dest_pixbuf);
+    int channels = gdk_pixbuf_get_n_channels(dest_pixbuf);
+    gboolean has_alpha = gdk_pixbuf_get_has_alpha(dest_pixbuf);
+
+    guchar *src = gdk_pixbuf_get_pixels(src_pixbuf);
+    guchar *dest = gdk_pixbuf_get_pixels(dest_pixbuf);
+
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int offset = (y * stride) + (x * channels);
+
+            guchar *src_p = src + offset;
+            guchar *dest_p = dest + offset;
+
+            double level;
+
+            level =   (src_p[0] * 0.393)
+                    + (src_p[1] * 0.769)
+                    + (src_p[2] * 0.189);
+
+            dest_p[0] = CLAMP(level, 0, 255);
+
+            level =   (src_p[0] * 0.349)
+                    + (src_p[1] * 0.686)
+                    + (src_p[2] * 0.168);
+
+            dest_p[1] = CLAMP(level, 0, 255);
+
+            level =   (src_p[0] * 0.272)
+                    + (src_p[1] * 0.534)
+                    + (src_p[2] * 0.131);
+
+            dest_p[2] = CLAMP(level, 0, 255);
 
             if (has_alpha)
                 dest_p[3] = src_p[3];
