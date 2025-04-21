5465ecdb53f4ef06061a5b4cc7cecce6ed8a12e0
hotnuma <hotnuma@gmail.com>
2025-04-21 12:25:41 +0200

    revert filters to faster ones

--- window.c
+++ window.c
@@ -143,8 +143,6 @@ static void _window_action_crop(VnrWindow *window, GtkWidget *widget);
 static void _window_action_resize(VnrWindow *window, GtkWidget *widget);
 static void _window_filter_grayscale(VnrWindow *window, GtkWidget *widget);
 static void _window_filter_sepia(VnrWindow *window, GtkWidget *widget);
-static void _filter_transform(GdkPixbuf *src_pixbuf, GdkPixbuf *dest_pixbuf,
-                              const float mat[4][4]);
 static void _window_view_set_static(VnrWindow *window, GdkPixbuf *pixbuf);
 
 // ----------------------------------------------------------------------------
@@ -2421,47 +2419,6 @@ static void _window_filter_grayscale(VnrWindow *window, GtkWidget *widget)
 
     GdkPixbuf *dest_pixbuf = _window_pixbuf_new(window);
 
-    const float mat[4][4] =
-    {
-        {0.3086, 0.3086, 0.3086, 0.0},
-        {0.6094, 0.6094, 0.6094, 0.0},
-        {0.0820, 0.0820, 0.0820, 0.0},
-        {0.0,    0.0,    0.0,    1.0},
-    };
-
-    _filter_transform(src_pixbuf, dest_pixbuf, mat);
-
-    _window_view_set_static(window, dest_pixbuf);
-}
-
-static void _window_filter_sepia(VnrWindow *window, GtkWidget *widget)
-{
-    (void) widget;
-
-    if (!window->can_edit)
-        return;
-
-    GdkPixbuf *src_pixbuf = uni_image_view_get_pixbuf(
-                                        UNI_IMAGE_VIEW(window->view));
-
-    GdkPixbuf *dest_pixbuf = _window_pixbuf_new(window);
-
-    const float mat[4][4] =
-    {
-        {0.393, 0.349, 0.272, 0.0},
-        {0.769, 0.686, 0.534, 0.0},
-        {0.189, 0.168, 0.131, 0.0},
-        {0.0,   0.0,   0.0,   1.0},
-    };
-
-    _filter_transform(src_pixbuf, dest_pixbuf, mat);
-
-    _window_view_set_static(window, dest_pixbuf);
-}
-
-static void _filter_transform(GdkPixbuf *src_pixbuf, GdkPixbuf *dest_pixbuf,
-                              const float mat[4][4])
-{
     int height = gdk_pixbuf_get_height(dest_pixbuf);
     int stride = gdk_pixbuf_get_rowstride(dest_pixbuf);
     int max_h = height * stride;
@@ -2478,6 +2435,67 @@ static void _filter_transform(GdkPixbuf *src_pixbuf, GdkPixbuf *dest_pixbuf,
     guchar *src = gdk_pixbuf_get_pixels(src_pixbuf);
     guchar *dest = gdk_pixbuf_get_pixels(dest_pixbuf);
 
+    gint64 t1 = g_get_real_time();
+
+    for (int y = 0; y < max_h; y+=stride)
+    {
+        for (int x = 0; x < max_w; x+=channels)
+        {
+            int offset = y + x;
+
+            guchar *src_color = src + offset;
+            guchar *dest_color = dest + offset;
+
+            float level =    src_color[0] * 0.3086
+                           + src_color[1] * 0.6094
+                           + src_color[2] * 0.0820;
+
+            dest_color[0] = (uint8_t) CLAMP(level, 0, 255);
+            dest_color[1] = dest_color[0];
+            dest_color[2] = dest_color[0];
+
+            if (has_alpha)
+                dest_color[3] = src_color[3];
+        }
+    }
+
+    gint64 t2 = g_get_real_time();
+    gint64 diff = t2 - t1;
+    printf("time = %d\n", (int) diff);
+
+    _window_view_set_static(window, dest_pixbuf);
+}
+
+static void _window_filter_sepia(VnrWindow *window, GtkWidget *widget)
+{
+    (void) widget;
+
+    if (!window->can_edit)
+        return;
+
+    GdkPixbuf *src_pixbuf = uni_image_view_get_pixbuf(
+                                        UNI_IMAGE_VIEW(window->view));
+
+    GdkPixbuf *dest_pixbuf = _window_pixbuf_new(window);
+
+    int height = gdk_pixbuf_get_height(dest_pixbuf);
+    int stride = gdk_pixbuf_get_rowstride(dest_pixbuf);
+    int max_h = height * stride;
+
+    int width = gdk_pixbuf_get_width(dest_pixbuf);
+    int channels = gdk_pixbuf_get_n_channels(dest_pixbuf);
+    int max_w = width * channels;
+
+    assert(stride == gdk_pixbuf_get_rowstride(src_pixbuf));
+    assert(channels == gdk_pixbuf_get_n_channels(src_pixbuf));
+
+    gboolean has_alpha = gdk_pixbuf_get_has_alpha(dest_pixbuf);
+
+    guchar *src = gdk_pixbuf_get_pixels(src_pixbuf);
+    guchar *dest = gdk_pixbuf_get_pixels(dest_pixbuf);
+
+    gint64 t1 = g_get_real_time();
+
     for (int y = 0; y < max_h; y+=stride)
     {
         for (int x = 0; x < max_w; x+=channels)
@@ -2489,19 +2507,34 @@ static void _filter_transform(GdkPixbuf *src_pixbuf, GdkPixbuf *dest_pixbuf,
 
             float level;
 
-            for (int i = 0; i < 3; ++i)
-            {
-                level =   mat[0][i] * src_color[0]
-                        + mat[1][i] * src_color[1]
-                        + mat[2][i] * src_color[2]
-                        + mat[3][i];
-                dest_color[i] = (uint8_t) CLAMP(level, 0, 255);
-            }
+            level =   (src_color[0] * 0.393)
+                    + (src_color[1] * 0.769)
+                    + (src_color[2] * 0.189);
+
+            dest_color[0] = (uint8_t) CLAMP(level, 0, 255);
+
+            level =   (src_color[0] * 0.349)
+                    + (src_color[1] * 0.686)
+                    + (src_color[2] * 0.168);
+
+            dest_color[1] = (uint8_t) CLAMP(level, 0, 255);
+
+            level =   (src_color[0] * 0.272)
+                    + (src_color[1] * 0.534)
+                    + (src_color[2] * 0.131);
+
+            dest_color[2] = (uint8_t) CLAMP(level, 0, 255);
 
             if (has_alpha)
                 dest_color[3] = src_color[3];
         }
     }
+
+    gint64 t2 = g_get_real_time();
+    gint64 diff = t2 - t1;
+    printf("time = %d\n", (int) diff);
+
+    _window_view_set_static(window, dest_pixbuf);
 }
 
 static void _window_view_set_static(VnrWindow *window, GdkPixbuf *pixbuf)
